79299dca067aa5119f9d0b3371d2feb49414808b
kuomin <tw.kuomin@gmail.com>
2022-12-10 18:29:53 +0800

    fix url

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url =  'https://etf-diy-kml.herokuapp.com' //dev
-const url =  'http://127.0.0.1:3085' // local
+const url =  'https://etf-diy-kml.herokuapp.com' //dev
+// const url =  'http://127.0.0.1:3085' // local
 
 const apiUsers = axios.create({
   baseURL: `${url}/users`,

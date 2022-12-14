f48fd8c1c5adc678020bffdd465c649439e08527
kuomin <tw.kuomin@gmail.com>
2022-12-14 20:57:57 +0800

    Fix compare render function

--- src/components/Compare.jsx
+++ src/components/Compare.jsx
@@ -58,111 +58,76 @@ const Compare = () => {
   useEffect(() => {
     setIsLoad(true);
     setAllData([]);
-    ETFCodes.map(async (code) => {
-      if (etfList.map((item) => item.code).indexOf(code) === -1) {
-        const diyData = diyList.filter((item) => item.name === code)[0];
-        (async () => {
-          try {
-            const res = await getDiyData(diyData.id, token);
-            const ETFName = res.diyData.name;
-            const FiveYearData = res.ETFAvgPriceArr.reverse();
-            const RoRData = periodRoR(FiveYearData);
-            const LineData = res.ETFAvgPriceArr;
-            const ContentData = res.diyData;
-            setAllData((prev) => [
-              ...prev,
-              { ETFName, code, RoRData, LineData, ContentData },
-            ]);
-          } catch (error) {
-            console.log(error);
-          }
-        })();
-        return;
-      }
 
+    const promises = ETFCodes.map(async (code) => {
       try {
-        const res = await getETFData(code);
-        const ETFName = res.itemName;
-        const FiveYearData = res.resultAll.reverse();
+        const diyData = diyList.find((item) => item.name === code);
+
+        let res;
+        if (diyData) {
+          res = await getDiyData(diyData.id, token);
+        } else {
+          res = await getETFData(code);
+        }
+
+        // 利用 res.diyData, res.ETFAvgPriceArr 判斷是否為 DIY ，並取得資料
+        const ETFName = res.diyData ? res.diyData.name : res.itemName;
+        const FiveYearData = res.ETFAvgPriceArr
+          ? res.ETFAvgPriceArr.reverse()
+          : res.resultAll.reverse();
         const RoRData = periodRoR(FiveYearData);
-        const LineData = res.resultAll;
-        const ContentData = res.ETFResultData;
-        setAllData((prev) => [
-          ...prev,
-          { ETFName, code, RoRData, LineData, ContentData },
-        ]);
+        const LineData = res.ETFAvgPriceArr
+          ? res.ETFAvgPriceArr
+          : res.resultAll;
+        const ContentData = res.diyData ? res.diyData : res.ETFResultData;
+
+        return { ETFName, code, RoRData, LineData, ContentData };
       } catch (error) {
         console.log(error);
       }
+    });
+
+    Promise.all(promises).then((results) => {
+      setAllData(results);
       setIsLoad(false);
     });
-    if (!ETFCodes.length) {
-      setIsLoad(false);
-    }
   }, [ETFCodes]);
 
   const totalLineData = useMemo(() => {
-    const allLineData = [...allData].map((item) => item.LineData);
-    const allLineDataFormat = allLineData.map((item) =>
-      LineChartDataFormat(item)
-    );
+    const allLineDataFormat = allData.reduce((acc, item) => {
+      const data = LineChartDataFormat(item.LineData);
+      data.datasets[0].label = item.ETFName;
+      data.datasets[0].borderColor = borderColor[acc.length];
+      data.datasets[0].backgroundColor = backgroundColor[acc.length];
+      acc.push(data);
+      return acc;
+    }, []);
 
-    allLineDataFormat.map((item, index) => {
-      item.datasets[0].label = allData[index].ETFName;
-      item.datasets[0].borderColor = borderColor[index];
-      item.datasets[0].backgroundColor = backgroundColor[index];
-      return item;
-    });
-
-    const totalLabel = filterDate(
-      [...allLineDataFormat].map((itme) => itme.labels)
-    );
-
-    const ans = {
-      labels: totalLabel[0],
+    return {
+      labels: filterDate(allLineDataFormat.map((itme) => itme.labels))[0],
       datasets: allLineDataFormat.map((item) => item.datasets[0]),
     };
-
-    return ans;
   }, [allData]);
-
-  // console.log(totalLineData);
 
   const totalRoRData = useMemo(() => {
     const allRoRData = [...allData].map((item) => item.RoRData);
-    const RoRTitle = [...allRoRData].map((item) =>
-      item.map((item) => item.name)
-    )[0];
-    const RoRData = [...allRoRData].map((item) =>
-      item.map((item) => item.data)
-    );
+    const RoRTitle = allRoRData[0]?.map((item) => item.name);
+    const RoRData = allRoRData.map((item) => item.map((item) => item.data));
 
-    const totalRoR = RoRTitle?.map((item, index) => {
-      const itemRoR = RoRData.map((i) => {
-        return i[index];
-      });
-      return [item, ...itemRoR];
-    });
-
-    return totalRoR;
+    return RoRTitle?.map((item, index) => [
+      item,
+      ...RoRData.map((i) => i[index]),
+    ]);
   }, [allData]);
 
-  const totalRatioData = useMemo(() => {
-    const allRatioData = [...allData].map((item) => item.ContentData);
-    const allRatioDataFormat = allRatioData.map((item) => ETFRatio(item));
+  const totalRatio = useMemo(() => {
+    const allRatioData = [...allData].map((item) => ETFRatio(item.ContentData));
 
     const totalRatioTitle = [1, 2, 3, 4, 5];
-    const totalRatio = totalRatioTitle?.map((item, index) => {
-      const itemRatio = allRatioDataFormat.map((i) => {
-        if (i[index] === undefined) {
-          return 0;
-        }
-        return i[index];
-      });
-      return [item, ...itemRatio];
-    });
-
-    return totalRatio;
+    return totalRatioTitle?.map((item, index) => [
+      item,
+      ...allRatioData.map((i) => i[index] || 0),
+    ]);
   }, [allData]);
 
   const totalPieData = useMemo(() => {
@@ -396,7 +361,7 @@ const Compare = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {totalRatioData?.map((item) => {
+                    {totalRatio?.map((item) => {
                       return (
                         <tr
                           key={item}
@@ -429,7 +394,13 @@ const Compare = () => {
                       );
                     })}
                     <tr className="border-b border-gray-200  text-d1">
-                      <th className="table-item">產業占比 Top1</th>
+                      <th
+                        className={`table-item ${
+                          totalPieData.length < 3 ? "text-left" : "text-center"
+                        }`}
+                      >
+                        產業占比 Top1
+                      </th>
                       {totalPieData?.map((item, index) => {
                         return (
                           <td
